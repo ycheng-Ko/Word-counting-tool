f9d5d34b33d607dc562fd436541d79aa7867a742
ycheng-Ko <ychengch@gmail.com>
2021-02-05 00:23:45 +0800

    testing

--- wordcount.py
+++ wordcount.py
@@ -1,35 +1,51 @@
 import sys
 import docx
+import datetime
 
 """Define Functions"""
 def readfile(fname):
     f = open(fname, "r")
-
+    
     return [line.rstrip('\n') for line in f]
 
+# function to seperate paragraphs
+def para_cutter(f): 
+    para = []
+
+    return para
 """Main"""
 fname = sys.argv[1]
+#outtype = sys.argv[2]
 
 para_list = readfile( fname )
 para_num = 0
 total_num = 0
+today = str(datetime.datetime.now().month) + str(datetime.datetime.now().day)
+output = docx.Document()
 
-#output = docx.Document()
 for i in range( len(para_list) ):   
     word =  para_list[i].split()
     if len(word) > 1:
-        if i < len(para_list) - 1:     
-            print( para_list[i] , end=' ')
+        if i < len(para_list) - 1:          # Other paragraph
+            output.add_paragraph( para_list[i]) #, end='')
             para_num += len(word)
         else:                               # The last paragraph
-            print( para_list[i], end=' ')
+            output.add_paragraph( para_list[i])#, end='')
             para_num += len(word)
-            print(f"({para_num})")
+            output.add_paragraph(f"({para_num})")
             total_num += para_num
     else:
         if i > 1:
-            print(f"({para_num})\n")
+            output.add_paragraph(f"({para_num})\n")
             total_num += para_num
             para_num = 0
         
-print(f"\n({total_num} words)")
+output.add_paragraph(f"\n({total_num} words)")
+
+if sys.argv[2] != None:
+    if sys.argv[2] == "-i":
+        output.save("iss" + today + ".docx")
+    elif sys.argv[2] == "-a":
+        output.save("arg" + today + ".docx")
+else:
+    output(today + ".docx")
